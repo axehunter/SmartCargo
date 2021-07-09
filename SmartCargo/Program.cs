3956fe0bc222ba680fc4b7414bc7dfa29b95b0d2
Кудреватых Татьяна <bts@skbkontur.ru>
2021-07-09 16:35:24 +0500

    Сохранение параметров грузов в массив массивов

--- SmartCargo/Program.cs
+++ SmartCargo/Program.cs
@@ -3,41 +3,43 @@ using System.Collections.Generic;
 
 namespace SmartCargo
 {
-	class Program
-	{
-		public static void Main()
-		{
+    class Program
+    {
+        public static void Main()
+        {
 
-			Console.WriteLine("Введите необходимую информацию о грузах — через ; без пробелов. Если вы закончили, введите слово END.");
+            Console.WriteLine("Введите необходимую информацию о грузах — через ; без пробелов. Если вы закончили, введите слово END.");
 
-			string[] cargoParams = new string[] { "NAME", "WEIGHT", "DATE_BEGIN", "DATE_END", "IMPORTANCE_LEVEL" };
+            string[] cargoParams = new string[] { "NAME", "WEIGHT", "DATE_BEGIN", "DATE_END", "IMPORTANCE_LEVEL" };
 
-			List<string> unsortedList = new List<string>();
+            List<string> cargoInfo = new List<string>();
 
-				while (true)
-					{
-						var line = Console.ReadLine();
-						if (line == "END") break;
-					unsortedList.Add(line);
-					}
+            while (true)
+            {
+                var line = Console.ReadLine();
+                if (line == "END") break;
+                cargoInfo.Add(line);
+            }
 
-				foreach (var item in unsortedList)
-					Console.WriteLine(item);
-				//этот foreach нужен будет после сортировки
+            foreach (var item in cargoInfo)
+                Console.WriteLine(item);
 
-				string [][] list2 = new string[unsortedList.Count][];
+            string[][] unsortedСargos = new string[cargoInfo.Count][];
+            
+            int index = 0;
+            foreach (var item in cargoInfo)
+            {
+                if (item == "")
+                    continue;
+                string[] param = item.Split(new[] { ';' });
+                unsortedСargos[index] = param;
+                index++;
+            }
 
-				foreach (var item in unsortedList)
-				{
-
-					string[] param = item.Split(new char[] { ';' });
-			}
-				
-// Следующие шаги:в цикле foreach сохранять Param, потому что в следующем цикле он пропадёт. Сохранить можно в list2
 
 
-		}
-	}
+        }
+    }
 }
 
 
